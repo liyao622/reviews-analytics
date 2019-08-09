5845286b828ee24323bb9ee72a9828dfca50f258
Leo Wu <leo0622@hotmail.com.tw>
2019-08-10 01:15:44 +0800

    add

---
+++ read.py
@@ -0,0 +1,14 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000000 == 0:
+		    print(len(data))
+print('檔案讀取完了, 總共有', len(data), '行')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每一筆留言平均長度= ', sum_len/len(data))
